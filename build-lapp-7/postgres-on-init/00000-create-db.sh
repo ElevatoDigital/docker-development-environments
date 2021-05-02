1cc9673242c3ab2d41dbe15271eaa91c89d1c7fb
Byron DeLaMatre <byron@delamatre.com>
2021-05-02 07:25:24 -0400

    postgres tweaks

--- build-lapp-7/postgres-on-init/00000-create-db.sh
+++ build-lapp-7/postgres-on-init/00000-create-db.sh
@@ -8,4 +8,6 @@ echo "Restoring DB using $file"
 psql -U postgres --dbname=postgres -c "CREATE USER $DBUSER;"
 psql -U postgres --dbname=postgres -c "CREATE DATABASE $DBNAME;"
 psql -U postgres --dbname=postgres -c "GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO $DBUSER;"
-psql -U postgres --dbname=$DBNAME < "$DUMPFILE" || exit 1
+if [ -f $DUMPFILE ]; then
+  psql -U postgres --dbname=$DBNAME < "$DUMPFILE" || exit 1
+fi
