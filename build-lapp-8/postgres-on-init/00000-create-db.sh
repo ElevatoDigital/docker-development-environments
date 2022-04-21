33635e253e354852ba646274b3c9f8fba0c2f062
Byron DeLaMatre <byron@delamatre.com>
2022-04-21 13:56:51 -0400

    adding lapp-8

---
+++ build-lapp-8/postgres-on-init/00000-create-db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DBNAME=development
+DBUSER=development
+DUMPFILE="/docker-entrypoint-initdb.d/import-dump"
+
+echo "Restoring DB using $file"
+psql -U postgres --dbname=postgres -c "CREATE USER $DBUSER;"
+psql -U postgres --dbname=postgres -c "CREATE DATABASE $DBNAME;"
+psql -U postgres --dbname=postgres -c "GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO $DBUSER;"
+if [ -f $DUMPFILE ]; then
+  psql -U postgres --dbname=$DBNAME < "$DUMPFILE" || exit 1
+fi
