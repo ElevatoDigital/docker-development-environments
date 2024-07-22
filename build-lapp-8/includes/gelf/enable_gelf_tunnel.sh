dbc2c0cf62f2ce8b6cb6eb68a0cfb7483a3d3d5f
byron delamatre <byron@delamatre.com>
2024-07-22 14:45:24 -0400

    tweaks

--- build-lapp-8/includes/gelf/enable_gelf_tunnel.sh
+++ build-lapp-8/includes/gelf/enable_gelf_tunnel.sh
@@ -1,3 +1,4 @@
 cp /root/.ssh/gelf.key.mounted /root/.ssh/gelf.key
 chmod 600 /root/.ssh/gelf.key
+dos2unix /root/.ssh/gelf.key
 ssh -o StrictHostKeyChecking=no -L 127.0.0.1:12201:127.0.0.1:12201 -p61107 gelf@graylog.3pth.com -f -N
